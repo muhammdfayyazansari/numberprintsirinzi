6da979f2c4590ffba6aee145072f8dd330f665f2
KhurramMustaqeem <itzkhurram367@gmail.com>
2022-09-25 19:40:55 +0500

    change

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 function generate(){
-    var num = document.querySelector('#num');
-    var count = document.querySelector('#count');
-    var show = document.querySelector('#show');
-    var display="";
-    for(var i=0; i<count.value; i++){
-        var multiply = parseInt(num.value) * (i+1);
+    let num = document.querySelector('#num');
+    let count = document.querySelector('#count');
+    let show = document.querySelector('#show');
+    let display="";
+    for(let i=0; i<count.value; i++){
+        let multiply = parseInt(num.value) * (i+1);
         console.log(multiply)
         display += `${num.value}&nbsp;&nbsp; x &nbsp;&nbsp;${i+1}&nbsp;&nbsp; = &nbsp;&nbsp;${multiply}&nbsp;&nbsp; <br>`
     }
